aed5c2f22a07a8649a4b652acae2b2747ceb2077
Santiago Urigüen <55363018+Santi-Uriguen@users.noreply.github.com>
2020-09-23 01:31:07 -0300

    Add files via upload

---
+++ scripts/transiciones.js
@@ -0,0 +1,50 @@
+//creo variables para almacenar los botones y sus ids
+let headBtn = document.getElementById("head");
+let HeadID = headBtn.id;
+
+let searchBtn = document.getElementById("search");
+let SearchID = searchBtn.id;
+
+let favBtn = document.getElementById("fav");
+let FavID = favBtn.id;
+
+let misGifosBtn = document.getElementById("mis_gifos");
+let misGifosID = misGifosBtn.id;
+
+//función para mostrar la sección clickeada y ocultar las demás
+
+function showSection(ShowClase, HidClase1, HidClase2, HidClase3) {
+  let showSection = document.getElementById(ShowClase + "_section");
+  let hideSection1 = document.getElementById(HidClase1 + "_section");
+  let hideSection2 = document.getElementById(HidClase2 + "_section");
+  let hideSection3 = document.getElementById(HidClase3 + "_section");
+
+  //condición: si el botón clickeado es search, no afecta a HEAD, caso contrario sí
+  if (ShowClase === "search") {
+    showSection.className = "searchShown";
+    hideSection1.className = "favHidden";
+    hideSection3.className = "mis_gifosHidden";
+  } else {
+    showSection.className = ShowClase + "Shown";
+    hideSection1.className = HidClase1 + "Hidden";
+    hideSection2.className = HidClase2 + "Hidden";
+    hideSection3.className = HidClase3 + "Hidden";
+  }
+}
+
+//agrego eventos a cada botón, llamando a la función y agregando los correspondientes parámetros
+headBtn.addEventListener("click", function () {
+  showSection(HeadID, FavID, SearchID, misGifosID);
+});
+
+favBtn.addEventListener("click", function () {
+  showSection(FavID, HeadID, SearchID, misGifosID);
+});
+
+searchBtn.addEventListener("click", function () {
+  showSection(SearchID, FavID, HeadID, misGifosID);
+});
+
+misGifosBtn.addEventListener("click", function () {
+  showSection(misGifosID, HeadID, FavID, SearchID);
+});
